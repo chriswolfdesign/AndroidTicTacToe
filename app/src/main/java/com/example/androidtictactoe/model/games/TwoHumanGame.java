bae46cb9f831566f5fd58b48ed7d7f6701040d65
Chris Wolf <chriswolfdesign@gmail.com>
2021-03-13 13:49:18 -0500

    Implemented TwoHumanGame.java

---
+++ app/src/main/java/com/example/androidtictactoe/model/games/TwoHumanGame.java
@@ -0,0 +1,25 @@
+package com.example.androidtictactoe.model.games;
+
+import com.example.androidtictactoe.model.Player;
+
+/**
+ * TwoHumanGame.java
+ *
+ * An instance of the game meant to manage two human players playing the game
+ *
+ * @author Chris Wolf
+ * @version 1.0.0 (March 13, 2021)
+ */
+public class TwoHumanGame extends Game {
+    /**
+     * Attempts to make the move the player has requested
+     * @param row the row of the square the player is trying to make a move on
+     * @param col the column of the square the player is trying to make a move on
+     * @param player the player who is trying to make the move
+     * @return false if the move is illegal, otherwise it returns true and makes the move
+     */
+    @Override
+    public boolean makeMove(int row, int col, Player player) {
+        return this.board.placePlayerAtSquare(row, col, player);
+    }
+}
