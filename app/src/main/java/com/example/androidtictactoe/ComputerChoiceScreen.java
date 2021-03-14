d641f60ecd2988b501cc41bbfa8deabbe3570411
Chris Wolf <chriswolfdesign@gmail.com>
2021-03-14 15:52:51 -0400

    Clicked the human vs. computer button on GameChoiceScreen now opens ComputerChoiceScreen

--- app/src/main/java/com/example/androidtictactoe/ComputerChoiceScreen.java
+++ app/src/main/java/com/example/androidtictactoe/ComputerChoiceScreen.java
@@ -10,5 +10,6 @@ public class ComputerChoiceScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_computer_choice_screen);
+        this.getSupportActionBar().hide();
     }
 }
