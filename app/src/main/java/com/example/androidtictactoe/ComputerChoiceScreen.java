d6944574f1543b4232e36e5539bffdd0a6f08a2b
Chris Wolf <chriswolfdesign@gmail.com>
2021-03-14 15:56:01 -0400

    Added temporary functionality for buttons on ComputerChoiceScreen

--- app/src/main/java/com/example/androidtictactoe/ComputerChoiceScreen.java
+++ app/src/main/java/com/example/androidtictactoe/ComputerChoiceScreen.java
@@ -3,6 +3,9 @@ package com.example.androidtictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ComputerChoiceScreen extends AppCompatActivity {
 
@@ -11,5 +14,19 @@ public class ComputerChoiceScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_computer_choice_screen);
         this.getSupportActionBar().hide();
+
+        Button playerXButton = (Button) findViewById(R.id.playerXButton);
+        playerXButton.setOnClickListener(this::playerXButtonClicked);
+
+        Button playerOButton = (Button) findViewById(R.id.playerOButton);
+        playerOButton.setOnClickListener(this::playerOButtonClicked);
+    }
+
+    public void playerXButtonClicked(View v) {
+        Toast.makeText(this, "You chose for the computer to play Player X", Toast.LENGTH_LONG).show();
+    }
+
+    public void playerOButtonClicked(View v) {
+        Toast.makeText(this, "You chose for the computer to play Player O", Toast.LENGTH_LONG).show();
     }
 }
