e2d5af018fa3a6a206a54dbc6248b37574e3de6f
Chris Wolf <chriswolfdesign@gmail.com>
2021-03-14 15:46:54 -0400

    Created portrait mode for ComputerChoiceScreen

---
+++ app/src/main/java/com/example/androidtictactoe/ComputerChoiceScreen.java
@@ -0,0 +1,14 @@
+package com.example.androidtictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComputerChoiceScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_computer_choice_screen);
+    }
+}
