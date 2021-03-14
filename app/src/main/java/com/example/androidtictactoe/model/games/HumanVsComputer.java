2e7c63a30b01f0d6402aadb5e7cc5cb0327f03c2
Chris Wolf <chriswolfdesign@gmail.com>
2021-03-14 12:46:16 -0400

    Implemented HumanVsComputer.boardValue

--- app/src/main/java/com/example/androidtictactoe/model/games/HumanVsComputer.java
+++ app/src/main/java/com/example/androidtictactoe/model/games/HumanVsComputer.java
@@ -1,6 +1,7 @@
 package com.example.androidtictactoe.model.games;
 
 import com.example.androidtictactoe.model.Player;
+import com.example.androidtictactoe.model.Values;
 
 public class HumanVsComputer extends Game {
     private Player computerPlayer;
@@ -16,10 +17,26 @@ public class HumanVsComputer extends Game {
 
     @Override
     public boolean makeMove(int row, int col) {
-        return false;
+        boolean result = this.makeMove(row, col, this.getCurrentPlayer());
+
+        if (result && !this.isGameOver()) {
+            this.makeComputerMove();
+        }
+
+        return result;
     }
 
     public void makeComputerMove() {
-        // TODO: Implement functionality for the minimax algorithm
+        // TODO: Implement functionality for the mini-max algorithm
+    }
+
+    public int boardValue() {
+        if (this.hasPlayerWon(Player.X)) {
+            return Values.PLAYER_X_WON;
+        } else if (this.hasPlayerWon(Player.O)) {
+            return Values.PLAYER_O_WON;
+        }
+
+        return Values.NEITHER_PLAYER_WON;
     }
 }
--- app/src/test/java/com/example/androidtictactoe/model/games/HumanVsComputerGameTest.java
+++ app/src/test/java/com/example/androidtictactoe/model/games/HumanVsComputerGameTest.java
@@ -1,4 +1,55 @@
 package com.example.androidtictactoe.model.games;
 
+import com.example.androidtictactoe.model.Player;
+import com.example.androidtictactoe.model.Values;
+import com.example.androidtictactoe.model.settings.BoardSettings;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class HumanVsComputerGameTest {
+
+    // boardValue tests
+
+    @Test
+    public void testBoardValueEmptyBoardNeitherPlayerWon() {
+        HumanVsComputer g = new HumanVsComputer(Player.X);
+        assertEquals(g.boardValue(), Values.NEITHER_PLAYER_WON);
+    }
+
+    @Test
+    public void testBoardValuesMovesMadeNeitherPlayerWon() {
+        HumanVsComputer g = new HumanVsComputer(Player.X);
+
+        assertTrue(g.makeMove(0, 0, Player.X));
+        assertTrue(g.makeMove(0, BoardSettings.BOARD_SIZE - 1, Player.O));
+        assertTrue(g.makeMove(BoardSettings.BOARD_SIZE - 1, 0, Player.X));
+        assertTrue(g.makeMove(BoardSettings.BOARD_SIZE - 1, BoardSettings.BOARD_SIZE - 1, Player.O));
+
+        assertEquals(g.boardValue(), Values.NEITHER_PLAYER_WON);
+    }
+
+    @Test
+    public void testBoardValuesXWonByRowsPlayerXWon() {
+        HumanVsComputer g = new HumanVsComputer(Player.X);
+
+        for (int i = 0; i < BoardSettings.BOARD_SIZE; i++) {
+            assertTrue(g.makeMove(0, i, Player.X));
+        }
+
+        assertEquals(g.boardValue(), Values.PLAYER_X_WON);
+    }
+
+    @Test
+    public void testBoardValuesOWonByColsPlayerOWon() {
+        HumanVsComputer g = new HumanVsComputer(Player.O);
+
+        for (int i = 0; i < BoardSettings.BOARD_SIZE; i++) {
+            assertTrue(g.makeMove(i, 0, Player.O));
+        }
+
+        assertEquals(g.boardValue(), Values.PLAYER_O_WON);
+    }
 }
